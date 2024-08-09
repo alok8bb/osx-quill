ea0281e307ccfed17dbf6886983884a797861f4d
alok8bb <alok8bb@gmail.com>
2024-08-09 18:33:24 +0530

    misc: add readme, screenshots and links in about page
    Signed-off-by: alok8bb <alok8bb@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+## osx-quill
+A simple macOS native app to display motivational quotes or any text on your menu bar. Choose from a selection of preloaded quote lists to stay inspired throughout your day or keep your own text.
+
+### Screenshots
+![Screenshot 1](/Screenshots/screenshot_1.png)
+<img src="Screenshots/screenshot_2.png" alt="Screenshot 2" style="width: 300px">
+
+### Todo
+- [x] allow custom text
+- [x] changing frequency 
+- [x] persist settings
+- [x] change quote on reboot/wake cycle (maybe in future)
+
+> [!NOTE]
+> I created this project for fun to explore SwiftUI, so I didn't spend much time on customizing the design and adding more extra features. It's inspired by an existing app that lets you put text on the MenuBar.
--- osx-quill/Pages/AboutView.swift
+++ osx-quill/Pages/AboutView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
+        VStack{
         VStack {
             Text("OSX-Quill").font(.title).bold().foregroundColor(.accentColor)
             Text("v0.1").font(.subheadline)
             Spacer().frame(height: 10)
             Text("osx-quill is a simple app that keeps you motivated by displaying inspiring quotes in your Mac's quick menu. It comes with a variety of preloaded quotes and will soon let you add your own custom lists and text. Stay focused and uplifted while you work!")    .multilineTextAlignment(.center)
-
+            
         }.padding().frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            HStack {
+                Link("GitHub", destination: URL(string: "https://github.com/alok8bb/osx-quill")!)
+                Link("Telegram", destination: URL(string: "https://broken_vortex.t.me/")!)
+            }
+                .padding()
+    }
     }
 }
